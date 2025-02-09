b5efd9f7baf496263cbaacd1667b0ef02dbf987b
Robert OGrady <turnstoashes350@gmail.com>
2025-02-09 08:29:23 -0500

    update

--- ChallengeTiles/ChallengeTiles.Server/obj/Debug/net8.0/ChallengeTiles.Server.AssemblyInfo.cs
+++ ChallengeTiles/ChallengeTiles.Server/obj/Debug/net8.0/ChallengeTiles.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengeTiles.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d582e39722d57e089e13590e2a8a71da6479237")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1582b67decaf2518a4e9874d560887c41ccb6df4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengeTiles.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengeTiles.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChallengeTiles/challengetiles.client/src/components/Hand.jsx
+++ ChallengeTiles/challengetiles.client/src/components/Hand.jsx
@@ -1,2 +1,27 @@
 //component for hand object
+import Tile from "./Tile";
+import PropTypes from "prop-types";
 
+const Hand = ({ tiles }) => {
+    return (
+        <div className="hand">
+            {tiles.map((tile) => (
+                <Tile key={tile.id} tile={tile} />
+            ))}
+        </div>
+    );
+};
+
+// Validate props
+Hand.propTypes = {
+    tiles: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            number: PropTypes.number.isRequired,
+            color: PropTypes.string.isRequired,
+            tileImageUrl: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
+
+export default Hand;
