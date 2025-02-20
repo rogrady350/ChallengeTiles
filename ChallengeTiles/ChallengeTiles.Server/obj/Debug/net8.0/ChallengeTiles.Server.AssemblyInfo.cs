7bbbb5fa363a0bb20ca353516591f823cdd4bc5e
Robert OGrady <turnstoashes350@gmail.com>
2025-02-20 10:13:01 -0500

    update structure

--- ChallengeTiles/ChallengeTiles.Server/obj/Debug/net8.0/ChallengeTiles.Server.AssemblyInfo.cs
+++ ChallengeTiles/ChallengeTiles.Server/obj/Debug/net8.0/ChallengeTiles.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengeTiles.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad371d40a16c4000ba822c10498f3029ea2f43d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81bcfedbb9a6875552ca1c2556c43afb9dd1112c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengeTiles.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengeTiles.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
