3dbd4e7bd6b87b0fef56a09127ed675db98d286f
Robert OGrady <turnstoashes350@gmail.com>
2025-03-25 14:34:00 -0400

    yml for github actions

--- players test script.sql
+++ players test script.sql
@@ -1,5 +1,5 @@
-INSERT INTO players (Username, Password, Name, Email) 
+INSERT INTO Players (Username, Password, Name, Email) 
 VALUES ('testuser', 'hashedpassword', 'Test User', 'testuser@example.com');
 
-INSERT INTO players (Username, Password, Name, Email) 
+INSERT INTO Players (Username, Password, Name, Email) 
 VALUES ('testuser2', 'hashedpassword2', 'Test User2', 'testuser2@example.com');
