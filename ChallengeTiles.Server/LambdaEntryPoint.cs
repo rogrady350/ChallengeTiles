4bcf0bf5d646feeacbe06e848157802ffaa85074
Robert OGrady <turnstoashes350@gmail.com>
2025-03-30 14:46:13 -0400

    lambda entry point update

--- ChallengeTiles.Server/LambdaEntryPoint.cs
+++ ChallengeTiles.Server/LambdaEntryPoint.cs
@@ -3,6 +3,7 @@ using Amazon.Lambda.AspNetCoreServer;
 using Amazon.Lambda.Core;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 using System.Text.Json;
 
 namespace ChallengeTiles.Server
@@ -13,6 +14,27 @@ namespace ChallengeTiles.Server
         {
             builder.Configure(app => { })
                    .ConfigureServices((context, services) => { });
+        }
+
+        public override async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandlerAsync(
+            APIGatewayHttpApiV2ProxyRequest request, ILambdaContext lambdaContext)
+        {
+            lambdaContext.Logger.LogLine("Lambda invoked by API Gateway.");
+
+            if (request == null)
+            {
+                lambdaContext.Logger.LogLine("Error: Received NULL request from API Gateway.");
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    StatusCode = 400,
+                    Body = "Bad Request: Null request received from API Gateway"
+                };
+            }
+
+            lambdaContext.Logger.LogLine($"Received request path: {request.RawPath ?? "NULL"}");
+            lambdaContext.Logger.LogLine($"Request Details: {JsonConvert.SerializeObject(request)}");
+
+            return await base.FunctionHandlerAsync(request, lambdaContext);
         }
     }
 }
