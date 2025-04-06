36b8f50a6f1234519d18d1f3a16a1e96d37a9b22
Robert OGrady <turnstoashes350@gmail.com>
2025-04-06 17:24:12 -0400

    Delete config.js

--- challengetiles.client/public/config.js
+++
@@ -1,9 +0,0 @@
-//API Gateway Stage Invoke URL
-window._env_ = {
-    VITE_API_URL: "https://r93ormq7xf.execute-api.us-east-2.amazonaws.com/dev"
-};
-
-/*EBS domain url
-window._env_ = {
-    VITE_API_URL: "http://challengetilesbackend-env.eba-9e2vzyts.us-east-2.elasticbeanstalk.com/api"
-};*/
